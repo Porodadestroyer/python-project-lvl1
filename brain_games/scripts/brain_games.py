509fb254167504cf474af128ac15c3f4216bb43a
Porodadestroyer <ruslan.savin.99@mail.ru>
2022-03-02 22:10:20 +0300

    move print function to cli.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
 
-from brain_games.cli import welcome_user
+from brain_games.cli import greetings, welcome_user
 
 
 def main():
-    print("Welcome to the Brain Games!")
+    greetings()
     welcome_user()
 
 
